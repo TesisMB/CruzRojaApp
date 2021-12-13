81788f282eb38e4826555c7ee5da41d41cf38485
unknown <maxigalancid@gmail.com>
2021-12-13 12:44:36 -0300

    Sprint 13 - 0.06
    Se agregaron los dummy chats, pero falta agregar la funcionalidad del button

--- src/app/pages/chat/groupchat/groupchat.page.ts
+++ src/app/pages/chat/groupchat/groupchat.page.ts
@@ -1,3 +1,4 @@
+import { TypeChatRooms } from './../../../models/TypeChatRooms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,6 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./groupchat.page.css'],
 })
 export class GroupChatPage implements OnInit {
+
+  messages = [
+    {
+      user: 'simon',
+      createdAt: 1554090856000,
+      msg: 'Hola, ¿como andas?'
+    },
+    {
+      user: 'max',
+      createdAt: 1554090856000,
+      msg: 'Todo bien, ¿y vos?'
+    },
+    {
+      user: 'simon',
+      createdAt: 1554090856000,
+      msg: 'Bien, por suerte'
+    }
+  ]
 
   constructor() { }
 
