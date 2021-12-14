8b3dcd877cc45a5d70e5e4b981c0a55c423eb1f3
Yoel <yoelsolca5@gmail.com>
2021-12-14 14:24:27 -0300

    Sprint 13 - 0.08

--- src/app/pages/chat/groupchat/groupchat.page.ts
+++ src/app/pages/chat/groupchat/groupchat.page.ts
@@ -1,34 +1,40 @@
 import { TypeChatRooms } from './../../../models/TypeChatRooms';
 import { Component, OnInit } from '@angular/core';
+import { HubConnectionBuilder, HubConnection } from '@aspnet/signalr';
 
 @Component({
   selector: 'app-groupchat',
   templateUrl: './groupchat.page.html',
   styleUrls: ['./groupchat.page.css'],
 })
-export class GroupChatPage implements OnInit {
+export class GroupChatPage  {
 
-  messages = [
-    {
-      user: 'simon',
-      createdAt: 1554090856000,
-      msg: 'Hola, ¿como andas?'
-    },
-    {
-      user: 'max',
-      createdAt: 1554090856000,
-      msg: 'Todo bien, ¿y vos?'
-    },
-    {
-      user: 'simon',
-      createdAt: 1554090856000,
-      msg: 'Bien, por suerte'
-    }
-  ]
 
-  constructor() { }
+  hubConnection: HubConnection;
 
-  ngOnInit() {
+  constructor() {
+    const builder = new HubConnectionBuilder();
+    this.hubConnection = builder.withUrl('https://localhost:5001/chat').build();
+
+    this.hubConnection.on('notificar', (mensaje) =>{
+
+      console.log(mensaje);
+
+    });
+    this.hubConnection.start();
+
   }
 
+  value() {
+    const btnSend = (document.getElementById('btnSend').addEventListener('click', (event) =>
+    {
+          const user = ((document.getElementById('user') as HTMLInputElement).value);
+          const message = (( document.getElementById('message') as HTMLInputElement).value);
+
+          console.log(user + ' ' + message);
+        })
+    );
+  };
 }
+
+
--- src/app/services/chat/chat.service.ts
+++ src/app/services/chat/chat.service.ts
@@ -13,36 +13,9 @@ import { environment } from 'src/environments/environment';
 export class ChatService extends DataService{
   private _hubConnection: HubConnection;
 
-  notificacion: any;
 
   constructor(http: HttpClient) {
     super(http, '/chatrooms');
-    this.createConnection();
-    //this.startConnection();
-  }
-
-  private createConnection() {
-    let builder = new HubConnectionBuilder();
-    builder.withUrl(environment +'chat').build();
-      console.log('entro createConnection');
-
-      /* this._hubConnection.on("ReceiveMessage", (Messages, user) =>{
-        user = 'yoel';
-
-
-        console.log(user + '' + Messages);
-
-        // let mje : any =JSON.parse(Messages);
-        // this.notificacion.emit(mje);
-
-      }); */
-      this._hubConnection.start();
-  }
-
-  sendMessage(message: Messages, room, user,) {
-    room = 'asd';
-    user = 'yoel';
-    this._hubConnection.invoke('SendMessage', message);
   }
 
 
