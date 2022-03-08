ec16bb0a6ffbfa5f51c4f5a2be0ffbcb4a62d8dd
unknown <maxigalancid@gmail.com>
2022-03-08 17:52:11 -0300

    Sprint 18 - Version 6

--- src/app/pages/deployment/deployment.page.ts
+++ src/app/pages/deployment/deployment.page.ts
@@ -50,7 +50,7 @@ export class DeploymentPage implements AfterViewInit, OnInit, OnDestroy  {
   }
 
   initMap(){
-    var map = L.map('map').setView([this.emergencies.locations.locationLatitude, this.emergencies.locations.locationLongitude], 10);
+    var map = L.map('map').setView([this.emergencies.locations.locationLatitude, this.emergencies.locations.locationLongitude], 9);
 
     L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
